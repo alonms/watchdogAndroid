6e244dad3c0c92d8cf44c5bfc4b4f2dd62144323
alon <apeleg2000@gmail.com>
2022-11-27 09:15:47 +0200

    autostart

--- app/src/main/java/com/sign/watchdog/MainActivity.java
+++ app/src/main/java/com/sign/watchdog/MainActivity.java
@@ -21,7 +21,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
     Button mButton;
-    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +28,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mButton = findViewById(R.id.button);
         mButton.setOnClickListener(this);
-        txt = findViewById(R.id.msg);
-
-        /*
-        // ws://10.0.2.16:38301
-        String ipAddress = "192.168.0.117";
-        InetSocketAddress inetSockAddress = new InetSocketAddress(ipAddress, 38301);
-        WebsocketServer wsServer = new WebsocketServer(inetSockAddress);
-        wsServer.setReuseAddr(true);
-        wsServer.start();
-        */
     }
 
 
@@ -48,16 +37,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         intent.setAction(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("https://dev.signage.me/installplayer/"));
         startActivity(intent);
-    }
-
-
-    public void startStartAWebApp(String startUrl, int sourceFlag, String packageName) {
-        Intent intent = new Intent("com.google.android.apps.chrome.webapps.WebappManager.ACTION_START_WEBAPP");
-        intent.putExtra("org.chromium.chrome.browser.webapp_url", startUrl); // String
-        intent.putExtra("org.chromium.chrome.browser.webapp_source", sourceFlag); // int
-        intent.putExtra("org.chromium.chrome.browser.webapk_package_name", packageName); // String
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
     }
 }
