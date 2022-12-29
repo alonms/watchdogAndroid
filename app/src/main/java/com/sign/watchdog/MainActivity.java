03077b1d85ea67148740fb1104d9502b9cb81d04
alon <apeleg2000@gmail.com>
2022-12-29 12:54:18 +0200

    deploy

--- app/src/main/java/com/sign/watchdog/MainActivity.java
+++ app/src/main/java/com/sign/watchdog/MainActivity.java
@@ -1,9 +1,14 @@
 package com.sign.watchdog;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
@@ -22,6 +27,8 @@ import java.util.Calendar;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity  {
+    public static final int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +36,21 @@ public class MainActivity extends AppCompatActivity  {
 
 
         if (!Settings.canDrawOverlays(getApplicationContext())) {
-            startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION));
+            ActivityResultLauncher<Intent> ativityResultLauncher = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (Settings.canDrawOverlays(getApplicationContext())) {
+                                deploy();
+                            }
+                        }
+                    });
+
+            //Create Intent
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+            ativityResultLauncher.launch(intent);
         }
-
-
-/*
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_BOOT_COMPLETED)){
-            Toast.makeText(this,"permission RECEIVE_BOOT_COMPLETED enabled",Toast.LENGTH_LONG).show();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_BOOT_COMPLETED}, 1);
-        }
-*/
-
 
 
 
@@ -89,15 +99,12 @@ public class MainActivity extends AppCompatActivity  {
 
     private void deploy() {
         String urlString = "https://galaxy.signage.me/installPlayer/install/deploy.html";
-        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(urlString));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlString));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setPackage("com.android.chrome");
-        try
-        {
-             startActivity(intent);
-        }
-        catch (ActivityNotFoundException ex)
-        {
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
         }
     }
 
