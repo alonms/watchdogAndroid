1ab9b773fa4a6f20f43b21c1895cc7507efee3aa
alon <apeleg2000@gmail.com>
2023-04-24 13:01:35 +0300

    requestUsageStats

--- app/src/main/java/com/sign/watchdog/BaseService.java
+++ app/src/main/java/com/sign/watchdog/BaseService.java
@@ -1,6 +1,8 @@
 package com.sign.watchdog;
 
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,6 +11,8 @@ import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.Map;
 
 
 public class BaseService extends Service {
@@ -38,4 +42,56 @@ public class BaseService extends Service {
     {
         return START_STICKY;
     }
+
+/*
+
+    public class AlarmThread extends Thread
+    {
+        private void updateLastTimeStamp() {
+            try {
+                UsageStatsManager usm = (UsageStatsManager)mContext.getSystemService("usagestats");
+                long currentTime = System.currentTimeMillis();
+                Map<String, UsageStats> appMap = usm.queryAndAggregateUsageStats(0, currentTime);
+                // UsageStats usageStats = appMap.get("com.sec.android.app.sbrowser");   // S21
+                UsageStats usageStats = appMap.get("com.android.chrome");
+                if (usageStats!=null) {
+                    //time1 = (currentTime - usageStats.getLastTimeStamp()) / 1000;
+                    time1 = (currentTime - usageStats.getLastTimeUsed()) / 1000;
+                }
+            } catch (Exception e) {
+                Log.e("Watchdog", e.getMessage());
+            }
+        }
+
+
+        public void run()
+        {
+            Log.d("Watchdog", "Thread started");
+            try {
+
+                mMessageHandler.sendEmptyMessage(MESSAGE_RESTART_PLAYER);
+                sleep(10000);
+            } catch (Exception e) {
+
+            }
+
+
+
+            while (true) {
+                try {
+                    Log.d("Watchdog", "time1="+String.valueOf(time1)+" time2="+String.valueOf(time2));
+                    if (time1 < time2) {
+                        mMessageHandler.sendEmptyMessage(MESSAGE_RESTART_PLAYER);
+                    }
+                    time2 = time1;
+                    sleep(5000);
+                    updateLastTimeStamp();
+                } catch (Exception e) {
+
+                }
+            }
+        }
+
+    }
+*/
 }
